559bccea36da62db11e5d173795ed024dc9848bd
Jason Sears <jsears4585@gmail.com>
2017-06-29 13:23:21 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
 
+  function getIt() {
+    $('p').on('click', function(e) {
+      alert("Hey!")
+    })
+  }
+
+  function frameIt() {
+    $('img').on('load', function(e) {
+      $(this).addClass('tasty')
+    })
+  }
+
+  function submitIt() {
+    $('form').on('submit', function(e) {
+      alert("Your form is going to be submitted now.")
+    })
+  }
+
+  function pressIt() {
+    $('#typing').on('keydown', function(key) {
+      if(key.which == 71){
+        alert('G was pressed!')
+      }
+    })
+  }
+
 $(document).ready(function(){
-
-// call functions here
-
+  getIt()
+  frameIt()
+  submitIt()
+  pressIt()
 });
